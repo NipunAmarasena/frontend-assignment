4506c61882ba019936a11ca63e1a532d5a9deda0
Amarasena, Nipun 341 <Nipun.Amarasena@sysco.com>
2021-10-25 18:25:19 +0530

    Remove react-errorboundary code

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { ErrorBoundary } from "react-error-boundary";
 
 import { getRequest } from "../util/http/makeRequest";
 import UserDetails from "./UserDetails";
